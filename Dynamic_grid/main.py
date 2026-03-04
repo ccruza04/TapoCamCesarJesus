81dba5f8afb3ea26f680cce9a3e13e1dde6e0129
ccruza04 <134280230+ccruza04@users.noreply.github.com>
2026-03-04 20:39:30 +0100

    Ajustar altura de filas para botones en listado de cámaras

--- Dynamic_grid/main.py
+++ Dynamic_grid/main.py
@@ -336,7 +336,8 @@ class CameraListPanel(QWidget):
         self.table.setSelectionMode(QTableWidget.SelectionMode.SingleSelection)
         self.table.setEditTriggers(QTableWidget.EditTrigger.NoEditTriggers)
         self.table.verticalHeader().setVisible(False)
-        self.table.verticalHeader().setMinimumSectionSize(12)
+        self.table.verticalHeader().setMinimumSectionSize(36)
+        self.table.verticalHeader().setDefaultSectionSize(40)
         self.table.horizontalHeader().setStretchLastSection(True)
 
         layout = QVBoxLayout(self)
